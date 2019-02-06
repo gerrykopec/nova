450486c4d5fe156cc7cf2fccdccac41d8831dc9c
Artom Lifshitz <alifshit@redhat.com>
2019-02-06 02:39:47 +0000

    [WIP needs more tests] Full NUMA live migration support
    To enble NUMA-aware live migration, this patch has the following
    changes:
    
    * If the instance has a NUMA topology, virt driver's
      check_can_live_migrate_source adds instance_numa_topology to
      migrate_data, indicating that it can perform a NUMA live migration.
    
    * compute manager's check_can_live_migrate_destination() creates a
      claim for the live migration if the source included
      instance_numa_topology in migrate_data.
    
    * post_live_migration_at_destination() applies the migration context
      to the instance if the live migration is successful (resources
      are already cleaned up by the update_available_resource call()).
    
    * If dst_numa_config is present in migrate_data (indicating that a
      NUMA live migration was performed), _rollback_live_migration()
      calls rollback_live_migration_at_destination() regardless of the
      do_cleanup boolean. This is to ensure the destination drops the
      migration context and claim if a claim was created.
    
    * rollback_live_migration_at_destination() drops the migration
      context from the instance.
    
    * If dst_numa_config is present in migrate_data,
      rollback_live_migration_at_destination() drops the claim if the
      live migration failed.
    
    Implements blueprint bp/numa-aware-live-migration
    Change-Id: I3c917796cb30d11e7db1e235ac1625d2a743aaa2

--- nova/tests/functional/compute/test_live_migration.py
+++ nova/tests/functional/compute/test_live_migration.py
@@ -16,7 +16,10 @@ import mock
 from oslo_utils.fixture import uuidsentinel as uuids
 
 from nova.compute import api as compute_api
+from nova import context
 from nova import exception
+from nova import objects
+from nova import test
 from nova.tests import fixtures as nova_fixtures
 from nova.tests.functional import integrated_helpers
 from nova.tests.unit import fake_notifier
@@ -41,14 +44,14 @@ class FakeCinderError(object):
             raise exception.CinderConnectionFailed(reason='Fake Cinder error')
 
 
-class LiveMigrationCinderFailure(integrated_helpers._IntegratedTestBase,
-                                 integrated_helpers.InstanceHelperMixin):
+class LiveMigrationTestBase(integrated_helpers._IntegratedTestBase,
+                            integrated_helpers.InstanceHelperMixin):
     api_major_version = 'v2.1'
     microversion = 'latest'
     USE_NEUTRON = True
 
     def setUp(self):
-        super(LiveMigrationCinderFailure, self).setUp()
+        super(LiveMigrationTestBase, self).setUp()
         fake_notifier.stub_notifier(self)
         self.addCleanup(fake_notifier.reset)
         # Start a second compte node (the first one was started for us by
@@ -57,6 +60,112 @@ class LiveMigrationCinderFailure(integrated_helpers._IntegratedTestBase,
         fake.set_nodes(['host2'])
         self.addCleanup(fake.restore_nodes)
         self.compute2 = self.start_service('compute', host='host2')
+
+
+class LiveMigrationClaimsTestBase(LiveMigrationTestBase):
+    """Base class for all live migration claims test cases.
+    """
+
+    def setUp(self):
+        super(LiveMigrationClaimsTestBase, self).setUp()
+        api_fixture = self.useFixture(nova_fixtures.OSAPIFixture(
+            api_version='v2.1'))
+        self.admin_api = api_fixture.admin_api
+        self.admin_api.microversion = 'latest'
+
+    def _boot_and_live_migrate_server(self, specify_dest=True,
+                                      expect_fail=False):
+        server = self.api.post_server({
+            'server': {
+                'flavorRef': 1,
+                'imageRef': '155d900f-4e14-4e4c-a73d-069cbf4541e6',
+                'name': 'live-migrate-claims-test',
+                'networks': 'none'}})
+        server = self._wait_for_state_change(self.api, server, 'ACTIVE')
+
+        cell1 = self.cell_mappings[test.CELL1_NAME]
+        with context.target_cell(context.get_admin_context(), cell1) as cctxt:
+            instance = objects.Instance.get_by_uuid(cctxt, server['id'])
+        self.source_host = server['OS-EXT-SRV-ATTR:host']
+        if self.source_host == self.compute.host:
+            src_mgr = self.compute
+            dst_mgr = self.compute2
+        else:
+            src_mgr = self.compute2
+            dst_mgr = self.compute
+        self.source_node = src_mgr._get_nodename(instance)
+        self.dest_host = dst_mgr.host
+        if expect_fail:
+            self.dest_host = src_mgr.host
+        post = {
+            'os-migrateLive': {
+                'host': self.dest_host,
+                'block_migration': 'auto',
+            }
+        }
+        if not specify_dest:
+            post['os-migrateLive']['host'] = None
+        self.admin_api.post_server_action(server['id'], post)
+        self._wait_for_server_parameter(
+            self.api, server, {'OS-EXT-SRV-ATTR:host': self.dest_host,
+                               'status': 'ACTIVE'})
+        self.dest_node = dst_mgr._get_nodename(instance)
+
+
+class LiveMigrationClaimsSuccessTest(LiveMigrationClaimsTestBase):
+    """Live migration claims test cases for when claims complete successfully.
+    This is a separate class because we need USE_CAST_AS_CALL here, otherwise
+    the migration will stay be in 'running' instead of 'completed'.
+    """
+
+    def test_live_migration_claims(self):
+        self._boot_and_live_migrate_server()
+        migrations = self.admin_api.get_migrations()
+        self.assertEqual(len(migrations), 1)
+        m = migrations[0]
+        self.assertEqual(m['source_compute'], self.source_host)
+        self.assertEqual(m['source_node'], self.source_node)
+        self.assertEqual(m['dest_compute'], self.dest_host)
+        self.assertEqual(m['dest_node'], self.dest_node)
+        self.assertEqual(m['status'], 'completed')
+
+
+class LiveMigrationClaimsFailureTest(LiveMigrationClaimsTestBase):
+    """Live migration claims test cases that test for failure scenarios. This
+    is a separate class because we don't want USE_CAST_AS_CALL here, to avoid
+    NoValidHost exceptions ending up in the API as 500 errors.
+    """
+    USE_CAST_AS_CALL = False
+
+    @mock.patch('nova.compute.resource_tracker.ResourceTracker._move_claim',
+                side_effect=exception.ComputeResourcesUnavailable('bork'))
+    def test_live_migration_with_dest_claim_fails(self, _):
+        self._boot_and_live_migrate_server(specify_dest=True, expect_fail=True)
+        migrations = self.admin_api.get_migrations()
+        self.assertEqual(len(migrations), 1)
+        m = migrations[0]
+        self.assertEqual(m['source_compute'], self.source_host)
+        self.assertEqual(m['source_node'], self.source_node)
+        self.assertIsNone(m['dest_compute'])
+        self.assertIsNone(m['dest_node'])
+        self.assertEqual(m['status'], 'error')
+
+    @mock.patch('nova.compute.resource_tracker.ResourceTracker._move_claim',
+                side_effect=exception.ComputeResourcesUnavailable('bork'))
+    def test_live_migration_without_dest_claim_fails(self, _):
+        self._boot_and_live_migrate_server(specify_dest=False,
+                                           expect_fail=True)
+        migrations = self.admin_api.get_migrations()
+        self.assertEqual(len(migrations), 1)
+        m = migrations[0]
+        self.assertEqual(m['source_compute'], self.source_host)
+        self.assertEqual(m['source_node'], self.source_node)
+        self.assertIsNone(m['dest_compute'])
+        self.assertIsNone(m['dest_node'])
+        self.assertEqual(m['status'], 'error')
+
+
+class LiveMigrationCinderFailure(LiveMigrationTestBase):
 
     # To get the old Cinder flow we need to hack the service version, otherwise
     # the new flow is attempted and CinderFixture complains about auth because
